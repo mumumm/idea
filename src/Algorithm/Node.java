5022cba3dd2a64da6d25fff5ae1fa911ad12525c
mumumm <1515440487@qq.com>
2020-05-21 22:53:06 +0800

    Commit Message

---
+++ src/Algorithm/Node.java
@@ -0,0 +1,34 @@
+package Algorithm;
+
+public class Node {
+
+    private Node previous=null;
+    private Node next=null;
+    private Object value;
+
+    public Node() {
+
+    }
+    public Node(Object value){
+        this.setValue(value);
+    }
+    public Object getValue() {
+        return value;
+    }
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
