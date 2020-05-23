207d9f7989c242afc563a01f44d9d8085f88c34f
mumumm <1515440487@qq.com>
2020-05-23 20:34:44 +0800

    Commit Message

--- src/test1/HelloWorld.java
+++ src/test1/HelloWorld.java
@@ -1,11 +1,18 @@
+package test1;
+
 public class HelloWorld {
     public static void main(String[] args){
         int aa= method(4,2);
-        System.out.println(aa);
+        //System.out.println(aa);
         int bb = method2(4);
-        System.out.println(bb);
+        //System.out.println(bb);
+        int cc=method3(4,0,1);
+        System.out.println(cc);
     }
     public static int method2(int time){
+         /*
+        该方法的作用为计算2+22+...+22...22
+        * */
         if(time==1){
             return 2;
         }
@@ -18,7 +25,7 @@ public class HelloWorld {
     }
     public static int method(int time,int numType){
         /*
-        该方法的作用为计算
+        该方法的作用为计算2+22+...+22...22
         * */
         if(time==1){
            return 2;
@@ -39,5 +46,17 @@ public class HelloWorld {
             num *=10;
         }
         return num;
+    }
+    public static int method3(int time,int change,int num){
+        /*
+        num为1~9之间
+        该方法的作用为计算num+(num*10+num)+(num*100+num*10+num)...+(num*10的n-1次方+...+num*10+num)
+        * */
+        if(time==0){
+            return 0;
+        }
+        change=change*10+num;
+        System.out.println(change);
+        return change+method3(--time,change,num);
     }
 }
